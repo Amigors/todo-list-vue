8bed61440e41bc7bf9f4114bf67fe5af0a392afb
Amigors <amigors994@gmail.com>
2021-11-18 00:42:25 +0300

    create app

---
+++ src/components/TodoList.vue
@@ -0,0 +1,15 @@
+<template>
+    <ul>
+        <TodoItem />
+    </ul>
+</template>
+
+<script>
+import TodoItem from '@/components/TodoItem'
+export default {
+    components: {
+        TodoItem
+    }
+    
+}
+</script>
